55fb23f6e2fa96d599fee39b5c576ae9f8479da3
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2024-08-13 16:23:59 -0400

    backend setup

---
+++ backend/src/main/java/com/revature/TravelPlanner/TravelPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.revature.TravelPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/revature/TravelPlanner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.TravelPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
