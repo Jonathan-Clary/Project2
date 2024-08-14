86d15289957a59b06672e0420f03490a3c0614d9
jasamra <jasprit45@gmail.com>
2024-08-14 07:10:56 -0700

    hotel

---
+++ src/main/java/com/revature/controllers/HotelController.java
@@ -0,0 +1,25 @@
+package com.revature.controllers;
+
+import com.revature.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hotels")
+@CrossOrigin
+public class HotelController {
+
+    private HotelService hotelService;
+
+    @Autowired
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+
+}
+
+
+
